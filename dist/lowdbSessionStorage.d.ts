974bb7656b202dd7dd9a9bc16805af6419cce60b
Artem Shinkaruk <artasend@gmail.com>
2020-05-05 21:02:29 +0700

    Update dist

--- dist/lowdbSessionStorage.d.ts
+++ dist/lowdbSessionStorage.d.ts
@@ -1,6 +1,14 @@
 import { ISessionStorage, ISession } from 'yandex-dialogs-sdk';
+import { LowdbSync } from 'lowdb';
+interface Schema {
+    sessions: [{
+        id: string;
+        session: any;
+    }];
+}
 export declare class LowDBSessionStorage implements ISessionStorage {
-    private db;
+    db?: LowdbSync<Schema>;
     constructor(filePath: string);
     getOrCreate(id: string): Promise<ISession>;
 }
+export {};
